394adc13af9648b3f430e4259cd1143a76a12c36
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-02-16 20:26:01 -0600

    Add empty files and creating abstract class DifferenceCalculatorBase with event, abstract int method called CalculateDifference and TriggerDifferenceCalculated method for Invoke event

---
+++ ExampleTwo/DifferenceCalculatorBase.cs
@@ -0,0 +1,19 @@
+// Base class enforcing a contract
+public abstract class DifferenceCalculatorBase {
+    
+    //Event triggered when the difference is calculated
+    public event Action<int>? OnDifferenceCalculated;
+    
+    // Abstract method to be implemented
+    public abstract int CalculateDifference(IReadOnlyList<int> numbers);
+    
+
+    // Method to trigger the event
+    protected void TriggerDifferenceCalculated(int difference){
+        OnDifferenceCalculated?.Invoke(difference);
+    }
+
+    // protected void TriggerDifferenceCalculated(int difference) => OnDifferenceCalculated?.Invoke(difference);
+
+
+}
